d77ffa695bdcd2a73a1c28cde98d0610d026544c
Sean McPherson <seanmcp25@gmail.com>
2019-03-28 23:40:33 -0400

    POC

--- src/app.js
+++ src/app.js
@@ -1,3 +1,24 @@
 var fs = require('fs')
+var data = require('../examples/data.json')
 
-fs.writeFileSync('test.html', '<html></html>')
+function buildHeadTag(metaData) {
+    return `
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>${metaData.title}</title>
+    </head>`
+}
+
+var site =
+`<!DOCTYPE html>
+<html lang="en">
+    ${buildHeadTag(data.meta)}
+    <body>
+        <div class="___johannes">
+            Hello world!
+        </div>
+    </body>
+</html>`
+
+fs.writeFileSync('dev/index.html', site)
