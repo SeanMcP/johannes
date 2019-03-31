bacf5684c729c86525402d061c2cb9063599ba43
Sean McPherson <seanmcp25@gmail.com>
2019-03-30 23:30:36 -0400

    Use better getId function

--- src/utils.js
+++ src/utils.js
@@ -1,18 +1,21 @@
-function getId(tag) {
-    var output = ''
-    if (tag) output += `${tag}-`
-    output += Math.floor(Math.random() * new Date().getTime())
-        .toString(16)
-        .slice(-5)
-    return output
-}
-
 function camelToKabob(string) {
     // From: https://gist.github.com/nblackburn/875e6ff75bc8ce171c758bf75f304707
     return string.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase()
 }
 
+function capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1)
+}
+
+function getId(tag) {
+    // From: https://gist.github.com/gordonbrander/2230317
+    var output = tag ? `${tag}-` : ''
+    output += Math.random().toString(36).substr(2, 5)
+    return output
+}
+
 module.exports = {
     camelToKabob,
+    capitalize,
     getId
 }
