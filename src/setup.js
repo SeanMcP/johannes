f5a11fcf1f536c86340b662ed6fd012187c030fd
Sean McPherson <seanmcp25@gmail.com>
2019-04-20 00:07:59 -0400

    Add help message

--- src/setup.js
+++ src/setup.js
@@ -1,14 +1,16 @@
 var minimist = require('minimist')
 var ENV = require('./constants').ENV
 
+var defaultConfig = {
+    cwd: process.cwd(),
+    env: ENV.prod,
+    input: './data.json',
+    output: './site',
+    filename: 'index.html'
+}
+
 function getConfig() {
-    var config = {
-        cwd: process.cwd(),
-        env: ENV.prod,
-        input: './data.json',
-        output: './site',
-        filename: 'index.html'
-    }
+    var config = { ...defaultConfig }
     var args = minimist(process.argv.slice(2))
 
     for (const arg in args) {
@@ -28,10 +30,31 @@ function getConfig() {
                 config.input = args[arg]
                 break
             }
+            case 'help':
+            case 'H': {
+                console.log(`Usage: johannes [options]
+
+Options:
+  --dev, -D ............... Set environment to 'development' 
+  --input, -I ............. Input file [default: ${defaultConfig.input}]
+  --output, -O ............ Build directory [default: ${defaultConfig.output}]
+  --filename, -F .......... Build file in directory [default: ${
+      defaultConfig.filename
+  }]
+  --help, -H .............. Output usage information
+  --version, -V ........... Output the version number
+                `)
+                process.exit(0)
+            }
             case 'output':
             case 'O': {
                 config.output = args[arg]
                 break
+            }
+            case 'version':
+            case 'V': {
+                console.log(require('../package.json').version)
+                process.exit(0)
             }
         }
     }
