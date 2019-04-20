7f6ddc28a2c3723db648422feaa60ad645aaced2
Sean McPherson <seanmcp25@gmail.com>
2019-04-19 23:47:38 -0400

    Add ifDev util

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+var ENV = require('./constants').ENV
+
 function camelToKabob(string) {
     // From: https://gist.github.com/nblackburn/875e6ff75bc8ce171c758bf75f304707
     return string.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase()
@@ -17,13 +19,22 @@ function getClass(condition, className, defaultValue = undefined) {
 function getId(tag) {
     // From: https://gist.github.com/gordonbrander/2230317
     var output = tag ? `${tag}-` : ''
-    output += Math.random().toString(36).substr(2, 5)
+    output += Math.random()
+        .toString(36)
+        .substr(2, 5)
     return output
+}
+
+function ifDev(fn) {
+    if (process.env.NODE_ENV === ENV.dev && typeof fn === 'function') {
+        fn()
+    }
 }
 
 module.exports = {
     camelToKabob,
     capitalize,
     getClass,
-    getId
+    getId,
+    ifDev
 }
