c1870246eddbd983efdad08fc430b0885c3552be
Sean McPherson <seanmcp25@gmail.com>
2019-04-20 00:11:03 -0400

    Run prettier

--- src/blocks.js
+++ src/blocks.js
@@ -1,18 +1,11 @@
-var {
-    capitalize,
-    getClass,
-    getId
-} = require('./utils')
+var { capitalize, getClass, getId } = require('./utils')
 var buildBlockCSS = require('./styles').buildBlockCSS
 var BLOCK_TYPE = require('./constants').BLOCK_TYPE
 
-function openBlockTag({
-    block,
-    className = '',
-    id,
-    style = ""
-}) {
-    var classes = `Block ${capitalize(block.type)} ${block.type} ${isStacked(block.optons)}`
+function openBlockTag({ block, className = '', id, style = '' }) {
+    var classes = `Block ${capitalize(block.type)} ${block.type} ${isStacked(
+        block.optons
+    )}`
     if (className) classes += ` ${className}`
     return `
     <section
@@ -28,15 +21,12 @@ function closeBlockTag() {
 
 var buildFunctionMap = {
     [BLOCK_TYPE.text]: buildTextBlock,
-    [BLOCK_TYPE.window]: buildWindowBlock,
+    [BLOCK_TYPE.window]: buildWindowBlock
 }
 
-function buildContent({
-    content,
-    theme
-}) {
+function buildContent({ content, theme }) {
     var styles = ''
-    var elements = content.reduce(function (accumulator, block) {
+    var elements = content.reduce(function(accumulator, block) {
         var id = getId(block.type)
 
         if (buildFunctionMap.hasOwnProperty(block.type)) {
@@ -62,16 +52,9 @@ function isStacked(options) {
     return ''
 }
 
-function buildTextBlock({
-    block,
-    id,
-    theme
-}) {
+function buildTextBlock({ block, id, theme }) {
     var {
-        data: {
-            heading,
-            paragraphs
-        }
+        data: { heading, paragraphs }
     } = block
     var styles = buildBlockCSS(id, block.styles, theme)
     var elements = openBlockTag({
@@ -80,7 +63,7 @@ function buildTextBlock({
     })
     if (heading) elements += `<h2>${heading}</h2>`
     if (paragraphs) {
-        paragraphs.forEach(function (paragraph) {
+        paragraphs.forEach(function(paragraph) {
             elements += `<p>${paragraph}</p>`
         })
     }
@@ -91,18 +74,16 @@ function buildTextBlock({
     }
 }
 
-function buildWindowBlock({
-    block,
-    id,
-    theme
-}) {
-    var {
-        options
-    } = block
-    var styles = buildBlockCSS(id, {
-        ...block.styles,
-        'background-image': `url(${block.data.url})`
-    }, theme)
+function buildWindowBlock({ block, id, theme }) {
+    var { options } = block
+    var styles = buildBlockCSS(
+        id,
+        {
+            ...block.styles,
+            'background-image': `url(${block.data.url})`
+        },
+        theme
+    )
     var elements = openBlockTag({
         block,
         className: getClass(options && options.parallax, 'Window--parallax'),
